7e579aa3d7f7b3771d89378cac33cf5a1321e8b5
steve <stevemighty2030@gmail.com>
2022-11-03 13:39:07 +0300

    completed the assignment

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE
+    bears (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        age INTEGER,
+        sex TEXT,
+        color TEXT,
+        temperament TEXT,
+        alive INTEGER
+    );
---
+++ lib/insert.sql
@@ -0,0 +1,39 @@
+INSERT INTO
+    bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Mr.Chocolate', 12, 'M', 'Brown', 'fun', 0);
+
+INSERT INTO
+    bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Rowdy', 12, 'M', 'Black', 'Sensitive', 1);
+
+INSERT INTO
+    bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Tabiths', 5, 'F', 'White', 'Playful', 1);
+
+INSERT INTO
+    bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Sergeant Brown', 18, 'M', 'Brown', 'Fearful', 0);
+
+INSERT INTO
+    bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Melissa', 10, 'F', 'Black', 'Protective', 1);
+
+INSERT INTO
+    bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Grinch', 12, 'M', 'Black', 'Angry', 1);
+
+INSERT INTO
+    bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Wendy', 5, 'F', 'White', 'Playful', 1);
+
+INSERT INTO
+    bears (name, age, sex, color, temperament, alive)
+VALUES
+    (NULL, 8, 'M', 'Black', 'Sensitive', 0);
